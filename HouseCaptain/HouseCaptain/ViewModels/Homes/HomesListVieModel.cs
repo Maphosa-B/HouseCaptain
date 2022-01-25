7584d57bfbc6d1963def911659754439b3d6a227
Maphosa-B <maphosabonkosi.bm@gmail.com>
2022-01-25 16:09:30 +0200

    Updated home Listing view model

--- HouseCaptain/HouseCaptain/ViewModels/Homes/HomesListVieModel.cs
+++ HouseCaptain/HouseCaptain/ViewModels/Homes/HomesListVieModel.cs
@@ -1,46 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using HouseCaptain.Entities;
-using HouseCaptain.Models.Homes;
-using HouseCaptain.Views.Homes;
-using HouseCaptain.Views.Shopping;
-using MvvmHelpers;
+﻿using HouseCaptain.Views.Homes;
 using MvvmHelpers.Commands;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+using MvvmHelpers;
+using HouseCaptain.Models.Homes;
+using HouseCaptain.Services.Version_1;
+using System.Collections.Generic;
+using Humanizer;
+using HouseCaptain.Views.Shopping;
 
 namespace HouseCaptain.ViewModels.Homes
 {
-    public class HomesListVieModel: MyBaseViewModel
+
+    public class HomesListVieModel : MyBaseViewModel
     {
-        public AsyncCommand AddHomePage { get; set; 
-        }public AsyncCommand GoToSelectedHomeCommand { get; set; }
+        private int IsNavigated = 0;
+        private HomeModel _SelectedHome;
 
-        public HomesEntity SelectedHome { get; set; }
-       
-        
-        public ObservableRangeCollection<HomeModel> HomesList { get; set; }
+        //Commands
+        public AsyncCommand GoToAddHomePageCommand { get; set; }
+        public AsyncCommand GetListOfHomesCommand { get; set; }
+        public AsyncCommand GoToSelectedHomeCommand { get; set; }
+        public AsyncCommand GoToHomeSettingsCommand { get; set; }
 
+        //Properties
+        public HomeModel SelectedHome
+        {
+            get => _SelectedHome;
+            set => SetProperty(ref _SelectedHome, value);
+        }
+        public ObservableRangeCollection<HomeModel> HomesList { set; get; }
+
+
+
+        //Constructor
         public HomesListVieModel()
         {
-            //Initializing commands
-            AddHomePage = new AsyncCommand(GoToAddHomeAsync);
+            HomesList = new ObservableRangeCollection<HomeModel>();
+
+            //Instantiating commands
+            GoToAddHomePageCommand = new AsyncCommand(GoToAddHomeAsync);
+            GetListOfHomesCommand = new AsyncCommand(GetHomesAsync);
             GoToSelectedHomeCommand = new AsyncCommand(GoToSelectedHomeAsync);
         }
 
-        async Task GoToAddHomeAsync() => await Shell.Current.GoToAsync(nameof(ShoppingHistoryPage));
+        //helper methods
+        async Task GoToAddHomeAsync()
+        {
+            IsBusy = true;
+            IsNotBusy = false;
+
+            if (IsNavigated == 0)
+            {
+                IsNavigated++;
+                await Shell.Current.GoToAsync(nameof(AddHomePage));
+                IsNavigated = 0;
+            }
+
+            IsBusy = false;
+            IsNotBusy = true;
+        }
+
+        private async Task GetHomesAsync()
+        {
+            IsBusy = true;
+            IsNotBusy = false;
+
+            //Clearing the list fisrt then retrive data from database
+            HomesList.Clear();
+
+            var HomesFromDb = await HomesService.GetAllHomesAsync();
+
+            List<HomeModel> LocalListOfHomes = new List<HomeModel>();
+            foreach (var i in HomesFromDb)
+            {
+                HomeModel tempHome = new HomeModel
+                {
+                    Id = i.Id,
+                    Name = i.Name.Humanize().Truncate(20, "...")
+                };
+                LocalListOfHomes.Add(tempHome);
+            }
+            HomesList.AddRange(LocalListOfHomes);
+
+            IsBusy = false;
+            IsNotBusy = true;
+        }
 
         async Task GoToSelectedHomeAsync()
         {
             IsBusy = true;
             IsNotBusy = false;
 
-            await Shell.Current.GoToAsync($"{nameof(ShoppingListPage)}?HomeId={SelectedHome.Id}");
+            var route = $"{nameof(ShoppingListPage)}?HomeId={_SelectedHome.Id}";
+            await Shell.Current.GoToAsync(route);
 
             IsBusy = false;
             IsNotBusy = true;
         }
-
     }
 }
